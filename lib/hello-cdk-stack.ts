1440fb73da0a2e38b718bc266ec61dbb939a86e2
youn410 <youn410@gmail.com>
2020-05-05 19:34:38 +0900

    first commit

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,46 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import * as rds from "@aws-cdk/aws-rds"
+
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3
+    });
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    const ecs_alb_fs = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster,
+      cpu: 512,
+      desiredCount: 6,
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")},
+      memoryLimitMiB: 2048,
+      publicLoadBalancer:  true
+    });
+    const ecs_alb_fs_sg = ecs_alb_fs.service.connections.securityGroups[0]
+
+    const rds_instance = new rds.DatabaseInstance(this, "MyRdsInstance", {
+      engine: rds.DatabaseInstanceEngine.MYSQL,
+      instanceClass: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      masterUsername: 'root',
+      vpc
+    });
+    rds_instance.connections.allowFrom(
+      ecs_alb_fs_sg,
+      new ec2.Port({
+        protocol: ec2.Protocol.TCP,
+        stringRepresentation: "for MySql",
+        fromPort: 3306,
+        toPort: 3306
+      })
+    );
   }
 }
